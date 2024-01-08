53a8d7c03ee81fa14482a6bd3ba4adf64eda44e6
Tzu-Ting Lo <tzutinglo.tw@gmail.com>
2024-01-08 14:05:03 +0800

    123

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ export default defineUserConfig({
   lang: "zh-TW",
   title: "你好， VuePress!",
   description: "這是我的第一個 VuePress 網站",
-  base: "/",
+  base: "/azusalotw.github.io/",
   theme: defaultTheme({
     // navbar: [
     //   // NavbarItem
